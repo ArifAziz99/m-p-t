a8a02a7937b93ff5cdce353dac42d71e06869010
Mohammad Arif Aziz <md.arifaziz24599@gmail.com>
2025-05-06 23:03:52 +0530

    some fixes

--- src/script.js
+++ src/script.js
@@ -5,3 +5,11 @@ const menu = document.getElementById('menu');
 toggle.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
+function openContactModal() {
+  document.getElementById('contactModal').classList.remove('hidden')
+}
+
+function closeContactModal() {
+  document.getElementById('contactModal').classList.add('hidden')
+}
