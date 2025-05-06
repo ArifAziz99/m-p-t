76c57e228c7928a47d2321e5702fbf1abfc54e2c
Mohammad Arif Aziz <md.arifaziz24599@gmail.com>
2025-05-06 23:52:11 +0530

    add mouse dot

--- src/script.js
+++ src/script.js
@@ -13,3 +13,30 @@ function openContactModal() {
 function closeContactModal() {
   document.getElementById('contactModal').classList.add('hidden')
 }
+
+
+//  mouse dot
+const mouseDot = document.querySelector('.mouse-dot');
+
+let mouseX = 0;
+let mouseY = 0;
+let dotX = 0;
+let dotY = 0;
+
+document.addEventListener('mousemove', (event) => {
+    mouseX = event.clientX + 80;
+    mouseY = event.clientY + 80;
+});
+
+function animate() {
+    // Move the dot gradually toward the mouse position
+    dotX += (mouseX - dotX) * 0.05;
+    dotY += (mouseY - dotY) * 0.05;
+
+    mouseDot.style.left = `${dotX}px`;
+    mouseDot.style.top = `${dotY}px`;
+
+    requestAnimationFrame(animate);
+}
+
+animate();
