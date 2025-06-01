6b6fe4b24488bf4fd52012f2d8c7b865d24ab29c
Mohammad Arif Aziz <md.arifaziz24599@gmail.com>
2025-06-01 20:46:12 +0530

    remove nav auto hide

--- src/script.js
+++ src/script.js
@@ -64,29 +64,9 @@ sections.forEach((section) => {
 	observer.observe(section);
 });
 
-
-// auto hide navbar on scroll
-let lastScrollY = window.scrollY;
-const navbar = document.getElementById("navbar");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > lastScrollY) {
-    // Scrolling down
-    navbar.style.top = "-80px"; // Hide the navbar
-  } else {
-    // Scrolling up
-    navbar.style.top = "0";
-  }
-  lastScrollY = window.scrollY;
-});
-
 // Initialize Lenis
 const lenis = new Lenis({
   autoRaf: true,
   syncTouch: true,
 });
 
-// // Listen for the scroll event and log the event data
-// lenis.on("scroll", (e) => {
-//   console.log(e);
-// });
--- src/script2.js
+++ src/script2.js
@@ -44,22 +44,6 @@ sections.forEach((section) => {
 	observer.observe(section);
 });
 
-
-// auto hide navbar on scroll
-let lastScrollY = window.scrollY;
-const navbar = document.getElementById("navbar");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > lastScrollY) {
-    // Scrolling down
-    navbar.style.top = "-80px"; // Hide the navbar
-  } else {
-    // Scrolling up
-    navbar.style.top = "0";
-  }
-  lastScrollY = window.scrollY;
-});
-
 // Initialize Lenis
 const lenis = new Lenis({
   autoRaf: true,
